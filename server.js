a8fe3933cf403b750287a426b911f45c09b19419
Kaan Büyükkatırcı <kaanbuyukkatirci@Kaan-MacBook-Air.local>
2024-12-01 17:49:46 +0300

    Updated

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-//const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
@@ -68,6 +68,6 @@ app.get("/api/products", async (req, res) => {
 });
 
 
-app.listen(process.env.PORT || 5001, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
